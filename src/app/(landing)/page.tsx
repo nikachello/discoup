5ffe1e80328803ab5560d08fcac4b815a973c57a
nikachello <nikain35@gmail.com>
2024-11-10 06:41:07 +0400

    discord clone

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,6 +2,9 @@ import { Heading } from "@/components/heading"
 import { MaxWidthWrapper } from "@/components/max-width-wrapper"
 import { Check } from "lucide-react"
 import { ShinyButton } from "@/components/shiny-button"
+import { MockDiscordUI } from "@/components/mock-discord-ui"
+import { AnimatedList, AnimatedListItem } from "@/components/ui/animated-list"
+import { DiscordMessage } from "@/components/discord-message"
 
 const Page = () => {
   return (
@@ -52,7 +55,46 @@ const Page = () => {
           </div>
         </MaxWidthWrapper>
       </section>
-      <section></section>
+      <section className="relative bg-brand-25 pb-4">
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <div className="relative mx-auto">
+          <MaxWidthWrapper classname="relative">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:m-4 lg:rounded-2xl lg:p-4">
+              <MockDiscordUI>
+                <AnimatedList>
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="DiscoUp Avatar"
+                    timestamp="Today at 12:00PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="New user is signed up"
+                    username="DiscoUp"
+                    content={{
+                      name: "Mateo Ortiz",
+                      email: "m.ortiz19@gmail.com",
+                    }}
+                  />
+
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="DiscoUp Avatar"
+                    timestamp="Today at 12:00PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="New user is signed up"
+                    username="DiscoUp"
+                    content={{
+                      name: "Mateo Ortiz",
+                      email: "m.ortiz19@gmail.com",
+                    }}
+                  />
+                </AnimatedList>
+              </MockDiscordUI>
+            </div>
+          </MaxWidthWrapper>
+        </div>
+      </section>
       <section></section>
       <section></section>
     </>
